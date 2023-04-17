8c5dc16f4e9579d88d41ced0a28a4f3feb934ba3
Daniel Lobato <daniel.lobato2001@gmail.com>
2023-04-17 00:13:06 -0300

    feat: adds about me

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -16,117 +16,133 @@ import { FaGithub } from "react-icons/fa";
 
 // Page Style
 import {
-  Header,
-  HeaderContent,
-  HeaderButtonsArea,
-  UserImage,
-  StackCards,
-  AboutMeArea,
-  AboutMeCardContent,
-  ProjectsArea,
-  ProjectsAreaSocialMediaMessage,
-  ProjectAreaWrapperColumns,
-  ProjectsAreaContent,
+	Header,
+	HeaderContent,
+	HeaderButtonsArea,
+	UserImage,
+	StackCards,
+	ProjectsArea,
+	ProjectsAreaSocialMediaMessage,
+	ProjectAreaWrapperColumns,
+	ProjectsAreaContent,
+	StacksArea,
 } from "./style";
 
 export const Home = (): JSX.Element => {
-  const gihubUrl = `https://github.com/${userData.githubUser}`;
-  const portfolioUrl = `https://github.com/${userData.githubUser}/my-portfolio`;
+	const gihubUrl = `https://github.com/${userData.githubUser}`;
+	const portfolioUrl = `https://github.com/${userData.githubUser}/my-portfolio`;
 
-  return (
-    <main id="home">
-      <Header>
-        <Container>
-          <HeaderContent>
-            <Flex>
-              <UserImage
-                src={`https://github.com/${userData.githubUser}.png`}
-                alt={userData.nameUser}
-                title={userData.nameUser}
-                width={"48px"}
-                height={"48px"}
-              />
-              <Text color="grey4">Opaaa, me chamo {userData.nameUser}</Text>
-            </Flex>
-            <Text as="p" color="grey2" type="body1">
-              Hey, allow to introduce my history, as you know, my name is Daniel
-              Lobato, I am a young amapaense who, since I was 15 years old, have
-              been interested in technology as a focus on the future... [ this
-              section is in progress... ]
-            </Text>
+	return (
+		<main id="home">
+			<Header>
+				<Container>
+					<HeaderContent>
+						<Flex>
+							<UserImage
+								src={`https://github.com/${userData.githubUser}.png`}
+								alt={userData.nameUser}
+								title={userData.nameUser}
+								width={"48px"}
+								height={"48px"}
+							/>
+							<Text color="grey4" type="heading3">
+								Opaaa, me chamo{" "}
+								<Text as="span" type="heading3" color="brand1">
+									{userData.nameUser}
+								</Text>
+							</Text>
+						</Flex>
+						<Text as="p" color="grey2" type="body1">
+							Sou um jovem amapaense, 22 anos. Aos 15 iniciei meus
+							interesses pela tecnologia ao ingressar no ensino
+							médio integrado Técnico em Informática. Desde então,
+							enxergo a Tecnologia da Informação como uma área do
+							conhecimento capaz de beneficiar empresas e a
+							sociedade e, ainda poder transversalizar outras
+							áreas agregando valor de forma conjunta em prol de
+							um mundo melhor.
+						</Text>
+						<Text as="p" color="grey2" type="body1">
+							Atualmente, estou finalizando a formação de
+							desenvolvedor full-stack pela Kenzie Academy Brasil.
+							De maneira concomitante, sou estudante de Ciência da
+							Computação pela Universidade Federal do Amapá -
+							UNIFAP e bolsista PICME pelo CNPq/CAPES.
+						</Text>
+						<StacksArea id="stacks">
+							<Text as="h1" type="heading1" color="grey5">
+								Criar e{" "}
+								<Text as="span" type="heading1" color="brand1">
+									desenvolver
+								</Text>{" "}
+								projetos é minha{" "}
+								<Text as="span" type="heading1" color="brand1">
+									paixão
+								</Text>
+							</Text>
 
-            <AboutMeArea>
-              <Container>
-                <AboutMeCardContent>
-                  <Text as="h2" type="heading4" color="grey4">
-                    About me
-                  </Text>
-                </AboutMeCardContent>
-              </Container>
-            </AboutMeArea>
-
-            <Text as="h1" type="heading1" color="grey5">
-              I{" "}
-              <Text as="span" type="heading1" color="brand1">
-                love
-              </Text>{" "}
-              creating and{" "}
-              <Text as="span" type="heading1" color="brand1">
-                developing
-              </Text>{" "}
-              projects
-            </Text>
-            <Text type="body1" color="grey2">
-              Discover here in this environment, created especially for you, all
-              my projects and technologies
-            </Text>
-            <HeaderButtonsArea>
-              <Button as="a" type="primary" href="#projects">
-                See Projects
-              </Button>
-              <Button as="a" type="outline" target="_blank" href={portfolioUrl}>
-                See my portfolio source code
-              </Button>
-              <Button
-                color="grey5"
-                as="a"
-                css={{ "&:hover": { color: "$grey1" } }}
-                type="circle"
-                target="_blank"
-                href={gihubUrl}
-              >
-                <FaGithub />
-              </Button>
-            </HeaderButtonsArea>
-            <StackCards id="stacks">
-              {stackData.map((stack, index) => (
-                <Stack key={index} title={stack.title} icon={stack.img} />
-              ))}
-            </StackCards>
-          </HeaderContent>
-        </Container>
-      </Header>
-      <ProjectsArea id="projects">
-        <Container>
-          <ProjectAreaWrapperColumns>
-            <ProjectsAreaSocialMediaMessage>
-              <Text as="h2" type="heading4" color="grey4">
-                My projects
-              </Text>
-              <Text as="p" type="body1" color="grey2">
-                Some of my{" "}
-                <Text as="span" color="brand5">
-                  side projects
-                </Text>
-              </Text>
-            </ProjectsAreaSocialMediaMessage>
-            <ProjectsAreaContent>
-              <Project />
-            </ProjectsAreaContent>
-          </ProjectAreaWrapperColumns>
-        </Container>
-      </ProjectsArea>
-      <Contacts />
-    </main>
-  );
+							<Text type="body1" color="grey2">
+								Descubra aqui neste ambiente, criado
+								especialmente para você, todos os meus projetos
+								e tecnologias.
+							</Text>
+							<HeaderButtonsArea>
+								<Button as="a" type="primary" href="#projects">
+									Ver Projetos
+								</Button>
+								<Button
+									as="a"
+									type="outline"
+									target="_blank"
+									href={portfolioUrl}
+								>
+									Ver código-fonte deste portfólio
+								</Button>
+								<Button
+									color="grey5"
+									as="a"
+									css={{ "&:hover": { color: "$grey1" } }}
+									type="circle"
+									target="_blank"
+									href={gihubUrl}
+								>
+									<FaGithub />
+								</Button>
+							</HeaderButtonsArea>
+							<StackCards id="stacks">
+								{stackData.map((stack, index) => (
+									<Stack
+										key={index}
+										title={stack.title}
+										icon={stack.img}
+									/>
+								))}
+							</StackCards>
+						</StacksArea>
+					</HeaderContent>
+				</Container>
+			</Header>
+			<ProjectsArea id="projects">
+				<Container>
+					<ProjectAreaWrapperColumns>
+						<ProjectsAreaSocialMediaMessage>
+							<Text as="h2" type="heading4" color="grey4">
+								Meus projetos
+							</Text>
+							<Text as="p" type="body1" color="grey2">
+								Alguns dos meus{" "}
+								<Text as="span" color="brand5">
+									projetos paralelos
+								</Text>
+							</Text>
+						</ProjectsAreaSocialMediaMessage>
+						<ProjectsAreaContent>
+							<Project />
+						</ProjectsAreaContent>
+					</ProjectAreaWrapperColumns>
+				</Container>
+			</ProjectsArea>
+			<Contacts />
+		</main>
+	);
 };
